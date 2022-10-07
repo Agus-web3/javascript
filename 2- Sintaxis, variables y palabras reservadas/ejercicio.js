e810b29313f17722e6b9a28b748985982aa96f81
Agus-web3 <108036702+Agus-web3@users.noreply.github.com>
2022-10-07 10:18:11 -0300

    Add files via upload

---
+++ 2- Sintaxis, variables y palabras reservadas/ejercicio.js
@@ -0,0 +1,14 @@
+let ejercicio_lista = {
+    miName: "Agustin",
+    miAge: 25,
+    soyDev: true,
+    miBirthday: new Date (1996,12,20) +1,
+    miFavoriteBook : {
+        title:"Deep Work",
+        author:"Cal Newport",
+        date: new Date(2016,0,5) +1,
+        url: "https://articulo.mercadolibre.com.ar/MLA-1124910990-libro-centrate-_JM#reco_item_pos=0&reco_backend=machinalis-homes-pdp-boos&reco_backend_type=function&reco_client=home_navigation-recommendations&reco_id=cf8bdab5-baca-4499-a55d-28ef610e07d0&c_id=/home/navigation-recommendations/element&c_element_order=1&c_uid=b1a83f2a-189e-4f34-8e51-a19b20f8577f",
+    }
+}
+
+
