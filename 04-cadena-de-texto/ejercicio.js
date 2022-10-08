34448dc2b20d8135a0209c5e8679cdc71ee640ef
Agus-web3 <108036702+Agus-web3@users.noreply.github.com>
2022-10-08 16:33:54 -0300

    Add files via upload

---
+++ 04-cadena-de-texto/ejercicio.js
@@ -0,0 +1,20 @@
+//Ejercicio Strings
+
+const firstName = 'Agustín';
+const surName = 'Sidler';
+let estudiante = `${firstName} ${surName}`;
+console.log(estudiante);
+let estudianteMayus = estudiante.toUpperCase();
+console.log(estudianteMayus);
+let estudianteMinus = estudiante.toLowerCase();
+console.log(estudianteMinus);
+let countSpaces = estudiante.length
+console.log(countSpaces)
+let firstLetter = estudiante.charAt(0);
+console.log(firstLetter);
+let lastLetter = estudiante.charAt(13);
+console.log(lastLetter);
+let withoutSpace = estudiante.replace(" ","");
+console.log(withoutSpace);
+let isName = estudiante.includes(`${firstName}`);
+console.log(isName);
