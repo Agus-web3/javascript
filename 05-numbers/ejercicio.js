e75b8cb199ffee533a0dbe692b12b23261514259
Agus-web3 <108036702+Agus-web3@users.noreply.github.com>
2022-10-09 16:19:31 -0300

    Add files via upload

---
+++ 05-numbers/ejercicio.js
@@ -0,0 +1,14 @@
+//Ejercicio
+
+let heightInt = 186;
+let heightFloat = 1.86;
+let weightFloat = 79.9;
+
+let heightInt_Up = Math.ceil(heightFloat);
+console.log(heightInt_Up);
+
+let weightInt_Down = Math.floor(weightFloat)
+console.log(weightInt_Down);
+
+let areSame = Number.MAX_VALUE === Number.MAX_VALUE + 1; 
+console.log(areSame); 
