ec105665bfc8ca58f0b0da079e4edf36e67097aa
Agus-web3 <108036702+Agus-web3@users.noreply.github.com>
2022-10-11 19:51:14 -0300

    Add files via upload

---
+++ 07-sets-y-objetos/conjuntos.js
@@ -0,0 +1,8 @@
+const family = ["Lautaro","Sonia","Beto","Mish","Agustín"];
+
+const familySet = new Set(family);
+
+familySet.add("Agustín");
+console.log(familySet);
+familySet.add("Javascript");
+console.log(familySet);
