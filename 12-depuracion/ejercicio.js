e871a9d195a5ee1c7d420e47cff656a45427ce7a
Agus-web3 <108036702+Agus-web3@users.noreply.github.com>
2022-10-21 21:08:55 -0300

    Add files via upload

---
+++ 12-depuracion/ejercicio.js
@@ -0,0 +1,12 @@
+
+let arrayFibo = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+function fibonacci (num) {
+    for (let i=0; i <= arrayFibo.length; i++) {
+        return console.log(arrayFibo.slice(0,num))
+       
+}
+}
+//}
+
+fibonacci(6)
